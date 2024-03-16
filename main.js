73a2294b4bb6484c09064139dc67c5def9abceed
kyy08 <kyyelzz1@gmail.com>
2024-03-16 21:22:27 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function sweetAlert() {
+    Swal.fire({
+      title: 'HOREE MENYALA ABANGKU',
+      text: 'Kamu berhasil login',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    });
+  }
+
+  function login() {
+    sweetAlert();
+  }
